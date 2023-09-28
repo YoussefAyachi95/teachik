2254b23076fa0bbcfe08ceebcd4c572e14f1bdb0
Youssef <youssef1995elam@gmail.com>
2023-09-28 12:56:41 +0200

    Add auth

--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -1,7 +1,9 @@
-import Image from 'next/image'
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <p>Hello World</p>
+    <div>
+      <UserButton afterSignOutUrl="/"/>
+    </div>
   )
 }
