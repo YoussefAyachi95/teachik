906c2751a89ac2c64a5701f9697fed7170fabdb3
Youssef <youssef1995elam@gmail.com>
2023-09-29 11:59:54 +0200

    Add teacher mode

--- app/(dashboard)/_components/SidebarRoutes.tsx
+++ app/(dashboard)/_components/SidebarRoutes.tsx
@@ -1,6 +1,7 @@
 "use client"
 
-import { Layout, Compass } from "lucide-react"
+import { usePathname } from "next/navigation"
+import { Layout, Compass, List, BarChart } from "lucide-react"
 import SidebarItem from "./SidebarItem"
 
 const guestRoutes = [
@@ -16,8 +17,25 @@ const guestRoutes = [
     }, 
 ]
 
+const teacherRoutes = [
+    {
+        icon: List,
+        label: "Courses",
+        href: "/teacher/courses"
+    },
+    {
+        icon: BarChart,
+        label: "Analytics",
+        href: "/teacher/analytics"
+    },
+]
+
 export default function SidebarRoutes() {
-    const routes = guestRoutes
+    const pathname = usePathname()
+    
+    const isTeacherPage = pathname?.includes("/teacher")
+    
+    const routes = isTeacherPage ? teacherRoutes : guestRoutes
 
     return (
         <div className="flex flex-col w-full">
--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -16,7 +16,7 @@ export default function DashBoardLayout({
             <Sidebar />
         </div>
 
-        <main className="md:pl-56 h-full">
+        <main className="md:pl-56 pt-[80px] h-full">
           {children}
         </main>
     </div>
