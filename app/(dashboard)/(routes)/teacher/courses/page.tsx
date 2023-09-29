723584307fedf1dfc6448d260bbfbdd23a0cca97
Youssef <youssef1995elam@gmail.com>
2023-09-29 12:38:26 +0200

    Add create course form

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,7 +1,15 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 
 export default function CoursesPage() {
   return (
-    <div>CoursesPage</div>
+    <div className="p-6">
+      <Link href="/teacher/create">
+        <Button>
+          New Course
+        </Button>
+      </Link>
+    </div>
   )
 }
