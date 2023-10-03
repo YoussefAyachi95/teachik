44a4c03364b2c8d21cb61da68ae9d7e05c2dd15b
Youssef <youssef1995elam@gmail.com>
2023-10-03 11:46:25 +0200

    Add description form

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -6,6 +6,7 @@ import IconBadge from "@/components/IconBadge"
 import { LayoutDashboard } from "lucide-react"
 
 import TitleForm from "./_components/TitleForm"
+import DescriptionForm from "./_components/DescriptionForm"
 
 export default async function CourseIdPage({
     params
@@ -62,6 +63,7 @@ export default async function CourseIdPage({
                 </h2>
               </div>
               <TitleForm initialData={course} courseId={course.id} />
+              <DescriptionForm initialData={course} courseId={course.id} />
             </div>
         </div>
     </div>
