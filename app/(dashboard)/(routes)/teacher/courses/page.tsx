07b752d83dfaaa5f3c6c5e1ff6981c14f98d7d2a
Youssef <youssef1995elam@gmail.com>
2023-10-08 13:32:19 +0200

    Add data table and columns UI for courses page

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,15 +1,32 @@
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
+import { db } from "@/lib/db";
 
-export default function CoursesPage() {
-  return (
+import { DataTable } from "./_components/DataTable";
+import { columns } from "./_components/Columns";
+
+const CoursesPage = async () => {
+  const { userId } = auth();
+
+  if (!userId) {
+    return redirect("/");
+  }
+
+  const courses = await db.course.findMany({
+    where: {
+      userId,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
+  return ( 
     <div className="p-6">
-      <Link href="/teacher/create">
-        <Button>
-          New Course
-        </Button>
-      </Link>
+      <DataTable columns={columns} data={courses} />
     </div>
-  )
+   );
 }
+ 
+export default CoursesPage;
