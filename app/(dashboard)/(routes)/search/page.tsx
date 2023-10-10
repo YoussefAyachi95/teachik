328868b760ba43a5cf524153a9757d19df9fd470
Youssef <youssef1995elam@gmail.com>
2023-10-10 13:12:26 +0200

    Add courses to search page and progress

--- app/(dashboard)/(routes)/search/page.tsx
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -1,22 +1,48 @@
 import { db } from "@/lib/db"
+import { getCourses } from "@/actions/getCourses"
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
 import { Categories } from "./_components/Categories"
 import { SearchInput } from "@/components/SearchInput"
+import { CoursesList } from "@/components/CoursesList";
 
-export default async function SearchPage() {
+interface SearchPageProps {
+  searchParams: {
+    title: string;
+    categoryId: string;
+  }
+};
+
+export default async function SearchPage({
+  searchParams
+}: SearchPageProps) {
+  const { userId } = auth();
+
+  if (!userId) {
+    return redirect("/");
+  }
+
+
   const categories = await db.category.findMany({
     orderBy: {
       name: "asc"
     }
   })
 
+  const courses = await getCourses({
+    userId,
+    ...searchParams,
+  })
 
   return (
     <>
       <div className="px-6 pt-6 md:hidden md:mb-0 block">
           <SearchInput />
       </div>
-      <div className="p-6">
+      <div className="p-6 space-y-4">
           <Categories items={categories} />
+          <CoursesList items={courses} />
       </div>
     </>
   )
